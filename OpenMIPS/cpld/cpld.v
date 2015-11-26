4db3872dc78eee459c8f8f760a763f0ccc0ec38a
xuweijie <xuweijie1995@sina.cn>
2015-11-26 22:03:11 +0800

    烧入CPLD的代码

--- OpenMIPS/cpld/cpld.v
+++ OpenMIPS/cpld/cpld.v
@@ -2,7 +2,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
-// 
+//
 // Create Date:    18:24:10 11/24/2015 
 // Design Name: 
 // Module Name:    cpld 
@@ -19,16 +19,27 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module cpld(
-	input		wire		IC0,
-	input		wire		IC1,
-	output	reg		RxD,
-	output	reg		TxD
-    );
-	 always @ (*) begin
-		RxD <= IC0;
-		TxD <= IC1;
-	 end
-	 
+	input		wire		RXD1,
+	input		wire		TXD1,
+	input		wire		RXD2,
+	input		wire		TXD2,
+	input		wire		PS2KB_CLOCK,
+	input		wire		PS2KB_DATA,
 
+	output		reg			IC0,
+	output		reg			IC1,
+	output		reg			IC2,
+	output		reg			IC3,
+	output		reg			IC4,
+	output		reg			IC5
+	);
+	always @ (*) begin
+		IC0 <= RXD1;
+		IC1 <= TXD1;
+		IC2 <= RXD2;
+		IC3 <= TXD2;
+		IC4 <= PS2KB_CLOCK;
+		IC5 <= PS2KB_DATA;
+	end
 
 endmodule
